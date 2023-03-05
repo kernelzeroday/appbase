247b9335c563236ded47aafb83dd5059ef230ec7
kelsey <enktal@gmail.com>
2023-03-05 11:13:31 -0800

    start

---
+++ front/App.tsx
@@ -0,0 +1,32 @@
+// core
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+// components & theme
+import { Spinner, NativeBaseProvider } from 'native-base';
+import { theme } from './theme';
+import { useColorScheme } from 'react-native';
+
+// redux & persister
+import { Provider as ReduxProvider } from 'react-redux';
+import { store, persistor } from './lib/redux/store';
+import { PersistGate } from 'redux-persist/es/integration/react';
+
+import { Root } from './Root';
+
+export default function App() {
+  const mode = useColorScheme();
+  // Changing initialColorMode to 'dark' or 'light' or system(mode)
+  theme.config.initialColorMode = mode;
+  return (
+    <NativeBaseProvider theme={theme}>
+      <ReduxProvider store={store}>
+        <PersistGate loading={<Spinner size="lg" />} persistor={persistor}>
+          <SafeAreaProvider>
+            <Root theme={theme} />
+          </SafeAreaProvider>
+        </PersistGate>
+      </ReduxProvider>
+    </NativeBaseProvider>
+  );
+}
