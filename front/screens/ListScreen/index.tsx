952a7e53bda5f3ca16d2c60a1f9c6872db0f15b8
kelsey <enktal@gmail.com>
2023-03-08 15:33:02 -0800

    plumbing no render yet

--- front/screens/ListScreen/index.tsx
+++ front/screens/ListScreen/index.tsx
@@ -13,19 +13,24 @@ import {
   Button,
 } from 'native-base';
 
-
 // navigation
 import { RootStackParamList } from '../../navigation/types';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useFocusEffect } from '@react-navigation/native';
-import { ApiService, UserModel, apiClient } from '../../lib/axios';
+import {
+  ApiService,
+  UserModel,
+  apiClient,
+  TimesheetModel,
+  ClockTimesResponseModel,
+} from '../../lib/axios';
 import Feather from '@expo/vector-icons/build/Feather';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'List'>;
 
 export const ListScreen: React.FC<Props> = () => {
   const [lists, setLists] = useState<UserModel[]>();
-  const [timesheet, setTimesheet] = useState<UserModel[]>();
+  const [timesheet, setTimesheet] = useState<TimesheetModel>();
   const onPressClockInButton = async () => {
     ApiService.clockIn().then((e) => {
       console.log(e);
@@ -33,17 +38,32 @@ export const ListScreen: React.FC<Props> = () => {
   };
   const onPressClockOutButton = async () => {
     ApiService.clockOut().then((e) => {
-      console.log(e); 
+      console.log(e);
     });
   };
 
   const onPressTimesheetRefreshButton = async () => {
-    ApiService.getTimesheet().then((e) => {
+    ApiService.getTimeCard().then((e) => {
       console.log(e);
-      
+      setTimesheet(e.data);
     });
-  }
+  };
 
+  function TimeCard(timesheet: { clock_times: any[] | null; }) {
+    //for each clock in and clock out, create a row
+    let array: JSX.Element[] = [];
+    //if timesheet.clock_times is null, return empty array
+    if (timesheet.clock_times == null) {
+      return <>{array}</>;
+    }
+    timesheet.clock_times.forEach((element) => {
+      //add the row to the list
+      array.push(<Text> {element.clock_in_time} </Text>);
+      array.push(<Text> {element.clock_out_time} </Text>);
+    });
+    //build array into a single element
+    return <>{array}</>;
+  }
 
   const onPressListItem = async (item: UserModel) => {
     // TODO: do something
@@ -91,23 +111,21 @@ export const ListScreen: React.FC<Props> = () => {
                 <Column justifyContent="center">
                   {/* use icon for message instead of user icon */}
                   <Icon
-
                     as={Feather}
                     color="emerald.500"
                     name="info"
                     size="sm"
                     mr={3}
                   />
-
                 </Column>
                 <Column>
                   <Heading fontSize="sm">
                     Name: Royal Administration Time Portal
                   </Heading>
                   <Text fontSize="xs">
-                  Welcome to our Time Portal Application, designed to make tracking your time and attendance simple and easy. 
-
-                    </Text>
+                    Welcome to our Time Portal Application, designed to make
+                    tracking your time and attendance simple and easy.
+                  </Text>
                 </Column>
               </Row>
             </Column>
@@ -183,37 +201,34 @@ export const ListScreen: React.FC<Props> = () => {
                 />
               </Column>
               <Column>
-              {/* punch in button */}
-              <Pressable
-                onPress={onPressClockInButton}
-                bg="emerald.500"
-                rounded="md"
-                _pressed={{
-                  bg: 'emerald.600',
-                }}
-                px={4}
-                py={2}
-
-              >              
-                <Text fontSize="sm">Punch In</Text>
-              </Pressable>
-              <Spacer>    </Spacer>
-              {/* punch out button */}
-              <Pressable
-                onPress={onPressClockOutButton}
-                bg="emerald.500"
-                rounded="md"
-                _pressed={{
-                  bg: 'emerald.600',
-                }}
-                px={4}
-                py={2}
-              >
-                <div></div>
-                <Text fontSize="sm">Punch Out</Text>
-              </Pressable>
-
-
+                {/* punch in button */}
+                <Pressable
+                  onPress={onPressClockInButton}
+                  bg="emerald.500"
+                  rounded="md"
+                  _pressed={{
+                    bg: 'emerald.600',
+                  }}
+                  px={4}
+                  py={2}
+                >
+                  <Text fontSize="sm">Punch In</Text>
+                </Pressable>
+                <Spacer> </Spacer>
+                {/* punch out button */}
+                <Pressable
+                  onPress={onPressClockOutButton}
+                  bg="emerald.500"
+                  rounded="md"
+                  _pressed={{
+                    bg: 'emerald.600',
+                  }}
+                  px={4}
+                  py={2}
+                >
+                  <div></div>
+                  <Text fontSize="sm">Punch Out</Text>
+                </Pressable>
               </Column>
             </Row>
           </Column>
@@ -247,69 +262,52 @@ export const ListScreen: React.FC<Props> = () => {
                 />
               </Column>
               <Column>
-              {/* date table */}
-              <Box
-        borderBottomWidth="1"
-        _light={{
-          borderColor: 'light.border',
-        }}
-        _dark={{
-          borderColor: 'dark.border',
-        }}
-        pl="4"
-        pr="5"
-        py="2"
-      >
-        <Row space={3} justifyContent="space-between">
-          <Column mr={1}>
-            <Row>
-              <Column justifyContent="center">
-                <table
-                  style={{
-                    borderCollapse: 'collapse',
-                    width: '100%',
+                {/* date table */}
+                <Box
+                  borderBottomWidth="1"
+                  _light={{
+                    borderColor: 'light.border',
                   }}
+                  _dark={{
+                    borderColor: 'dark.border',
+                  }}
+                  pl="4"
+                  pr="5"
+                  py="2"
                 >
-                  <tr>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                    <th>Sunday</th>
-                  </tr>
-                  <tr>
-                    <td>8:00am</td>
-                    <td>8:00am</td>
-                    <td>8:00am</td>
-                    <td>8:00am</td>
-                    <td>8:00am</td>
-                    <td>8:00am</td>
-                    <td>8:00am</td>
-                  </tr>
-                  <tr>
-                    <td>5:00pm</td>
-                    <td>5:00pm</td>
-                    <td>5:00pm</td>
-                    <td>5:00pm</td>
-                    <td>5:00pm</td>
-                    <td>5:00pm</td>
-                    <td>5:00pm</td>
-                  </tr>
-                </table>
-              </Column>
-            </Row>
-          </Column>
-        </Row>
-      </Box>
-              </Column>
-            </Row>
-          </Column>
-        </Row>
-      </Box>
-      
+                  <Row space={3} justifyContent="space-between">
+                    <Column mr={1}>
+                      <Row>
+                        <Column justifyContent="center">
+                          {/* get timesheet button*/}
+                          <Pressable
+                            onPress={onPressTimesheetRefreshButton}
+                            bg="emerald.500"
+                            rounded="md"
+                            _pressed={{
+                              bg: 'emerald.600',
+                            }}
+                            px={4}
+                            py={2}
+                          >
+                            <div></div>
+                            <Text fontSize="sm">Get Timesheet</Text>
+                          </Pressable>
 
+                          {/* table for timesheet data from value timesheet */}
+                          <TimeCard timesheet={timesheet}>
+                            {' '}
+                          </TimeCard>
+                        </Column>
+                      </Row>
+                    </Column>
+                  </Row>
+                </Box>
+              </Column>
+            </Row>
+          </Column>
+        </Row>
+      </Box>
     </Center>
   );
 };
