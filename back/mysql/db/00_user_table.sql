b876b3d143ed56fa74ce6b36adffebcdb163a198
one <one@Libcy.io>
2023-03-11 20:47:35 -0600

    added timecard for admin to display ALL users timesheets, and created models for admin sign up and admin login.

--- back/mysql/db/00_user_table.sql
+++ back/mysql/db/00_user_table.sql
@@ -1,3 +1,16 @@
+CREATE TABLE `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `first_name` varchar(256) NOT NULL,
+  `last_name` varchar(256) NOT NULL,
+  `password_hash` char(60) NOT NULL,
+  `admin_timezone` varchar(256) NOT NULL,
+  `admin_role` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+);
+
+
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(256) NOT NULL,
@@ -5,6 +18,7 @@ CREATE TABLE `user` (
   `last_name` varchar(256) NOT NULL,
   `password_hash` char(60) NOT NULL,
   `user_timezone` varchar(256) NOT NULL,
+  'user_role' varchar(256) NOT NULL,
   `clock_in_times` json DEFAULT NULL,
   `clock_out_times` json DEFAULT NULL,
   PRIMARY KEY (`id`),
