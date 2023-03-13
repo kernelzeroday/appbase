06b534e6bb43b08f546dfdcc1ff08fdd94e232e5
kelsey <enktal@gmail.com>
2023-03-13 13:15:10 -0700

    Revert "axios model update"
    This reverts commit c1edb279666bf5d28f9c796f3a351261eee44e23.

--- frontend/lib/axios/endpoints.ts
+++ frontend/lib/axios/endpoints.ts
@@ -1,23 +1,22 @@
 import http from './http';
-//import {
-  //UserModel,
-  //SigninModel,
-  //Token,
-  //UserRegisterOrUpdateModel,
-//} from './model';
-
+import {
+  UserModel,
+  SigninModel,
+  Token,
+  UserRegisterOrUpdateModel,
+} from './model';
 import { UserUpdateRequestModel, TokenModel, SignInRequestModel, UserResponseModel } from './apimodels';
 
 class ApiService {
-  signup(data: UserUpdateRequestModel) {
+  signup(data: UserRegisterOrUpdateModel) {
     return http.post<UserResponseModel>('/v1/signup', data);
   }
 
-  signin(data: SignInRequestModel) {
-    return http.post<{ user: UserResponseModel; tokenModel: TokenModel }>('/v1/signin', data);
+  signin(data: SigninModel) {
+    return http.post<{ user: UserResponseModel; token: Token }>('/v1/signin', data);
   }
 
-  updateUser(data: UserUpdateRequestModel) {
+  updateUser(data: UserRegisterOrUpdateModel) {
     return http.post<UserResponseModel>('/v1/user/update', data);
   }
 
