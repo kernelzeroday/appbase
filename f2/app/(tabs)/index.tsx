72ab2466b8fdd917766c091d617f0369c820eae4
Nick <nwarzins@gmail.com>
2023-03-19 23:01:53 -0400

    Use new login for index page, fix login text color

--- f2/app/(tabs)/index.tsx
+++ f2/app/(tabs)/index.tsx
@@ -1,112 +1,132 @@
-import { Pressable, StyleSheet, Button, TextInput } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  Container,
+  Button,
+  Input,
+  Text,
+  NativeBaseProvider,
+  Image,
+} from "native-base";
+import { FontAwesome } from "@expo/vector-icons";
+import { OpenAPI } from '../client/core/OpenAPI';
 
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
 import { DefaultService } from "../client";
-import React from "react";
-import { Navigate } from "react-router-dom";
 
-async function ClientTestFunc() {
-  const res = await DefaultService.notSecretDataApiNotSecretGet();
-  console.log(res);
-}
+const SignInScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-// login handler with navigation
-async function LoginHandler(email: any, password: any) {
-  const res = await DefaultService.signinApiV1SigninPost({user_email: email, user_password: password});
-  //navigate home page in react
-  // if fail, display error message
-}
+  const handleSignIn = async () => {
+    const res = await DefaultService.signinApiV1SigninPost({user_email: email, user_password: password});
+    OpenAPI.TOKEN = res.token.access_token;
+    // TODO error handling
 
-export default function TabOneScreen() {
+    // navigate to signin landing page - store token in state if not error
+
+    //   /Dashboard
+    // TODO: implement sign in logic
+
+  };
+
   return (
-    <View style={styles.container}>
-      {/* Login Section */}
-      <Text style={styles.title}>Royal Admin Time Portal Login</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      {/* test button */}
-      {/* <Button title="Test" onPress={ClientTestFunc} />  */}
+    <NativeBaseProvider>
+      <Container style={styles.container} >
+        {/* logo from assets  */}
+        {/* <Image
+            source={require("../../assets/images/logo.jpg")}
+            style={{ width: 200, height: 200 }}
+            /> */}
+        {/* vector logo from font awesome */}
+        {/* <FontAwesome name="user-circle" size={200} color="black" /> */}
 
-{/* medium text area */}
-      <View style={{ width: "70%" }}>
-        {/* medium text */}
-        {/* center it */}
-        <Text style={{ fontSize: 15, textAlign: "center" }}>
-          Please enter your login credentials below to access the Royal Admin
-          Time Portal.
-        </Text>
-      </View>
-      {/* spacer */}
-      <View style={{ height: 10 }} />
+        <View
+          style={{ flexDirection: "row", backgroundColor: "#eee", padding: 20 }}
+        >
+          <FontAwesome name="clock-o" size={100} color="black" />
+          {/* padding */}
+            <View style={{ width: 20 }} />
+          <FontAwesome name="vcard" size={100} color="black" />
+        </View>
 
+        {/* Login Section */}
+        <Text style={styles.title}>Royal Admin Time Portal Login</Text>
+        {/* test button */}
+        {/* <Button title="Test" onPress={ClientTestFunc} />  */}
 
-      {/* input areas for email and password */}
-
-      {/* email input */}
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        placeholder="  Email"
-      />
-
-      {/* spacer */}
-      <View style={{ height: 10 }} />
-      {/* password input */}
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        placeholder="  Password"
-      />
-
-      {/* padding */}
-      <View style={{ height: 10 }} />
-
-      {/* login button  */}
-      <Button title="Login" onPress={ClientTestFunc} />
-
-      {/* Warnings */}
-      <View style={{ height: 10 }} />
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      {/* container area, 70% width of screen */}
-      <View style={{ width: "70%" }}>
-        {/* very small text */}
-        <Text style={{ fontSize: 11 }}>
-        Welcome to the Royal Admin Time Portal. We take the security and privacy
-        of our users very seriously, and have implemented various measures to
-        ensure the protection of your information. Please note that this is a
-        secure web application, and access is limited to authorized personnel
-        only. We kindly ask that you keep your login credentials confidential
-        and do not share them with anyone. Our system employs advanced
-        encryption technology to safeguard sensitive data, and our team
-        continually monitors the application for any suspicious activity. If you
-        encounter any issues with your login or have any questions about our
-        security measures, please do not hesitate to contact our support team
-        for assistance. Thank you for using the Royal Admin Time Portal.
-      </Text>
-      </View>
-    </View>
+        {/* medium text area */}
+        <View style={{ width: "70%" }}>
+          {/* medium text */}
+          {/* center it */}
+          <Text style={{ fontSize: 15, textAlign: "center", color: "#eee" }}>
+            Please enter your login credentials below to access the Royal Admin
+            Time Portal.
+          </Text>
+        </View>
+        {/* spacer */}
+        <View style={{ height: 10 }} />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+          keyboardType="email-address"
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+        <Button onPress={handleSignIn} style={styles.button}>
+          <Text>Sign In</Text>
+        </Button>
+        {/* Warnings */}
+        <View style={{ height: 10 }} />
+        {/* container area, 70% width of screen */}
+        <View style={{ width: "70%" }}>
+          {/* very small text */}
+          <Text style={{ fontSize: 11, color: "#eee"  }}>
+            Welcome to the Royal Admin Time Portal. We take the security and
+            privacy of our users very seriously, and have implemented various
+            measures to ensure the protection of your information. Please note
+            that this is a secure web application, and access is limited to
+            authorized personnel only. We kindly ask that you keep your login
+            credentials confidential and do not share them with anyone. Our
+            system employs advanced encryption technology to safeguard sensitive
+            data, and our team continually monitors the application for any
+            suspicious activity. If you encounter any issues with your login or
+            have any questions about our security measures, please do not
+            hesitate to contact our support team for assistance. Thank you for
+            using the Royal Admin Time Portal.
+          </Text>
+        </View>
+      </Container>
+    </NativeBaseProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    marginBottom: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: "bold",
+    marginBottom: 20,
+    color: "#eee" ,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  input: {
+    marginBottom: 10,
+    color: "#eee" ,
+  },
+  button: {
+    marginTop: 20,
   },
 });
+
+export default SignInScreen;
