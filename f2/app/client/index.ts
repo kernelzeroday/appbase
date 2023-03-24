693e3549017442fd2ca42bf13772e7f729cb0a3c
kelsey <enktal@gmail.com>
2023-03-23 18:47:59 -0700

    update client

--- f2/app/client/index.ts
+++ f2/app/client/index.ts
@@ -1,10 +1,6 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export { ApiError } from './core/ApiError';
-export { CancelablePromise, CancelError } from './core/CancelablePromise';
-export { OpenAPI } from './core/OpenAPI';
-export type { OpenAPIConfig } from './core/OpenAPI';
 
 export type { AdminSignInRequestModel } from './models/AdminSignInRequestModel';
 export type { AdminSignUpRequestModel } from './models/AdminSignUpRequestModel';
