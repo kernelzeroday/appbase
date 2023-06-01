523452b4254609521959c2231f089f36f8fb0a01
one <one@Libcy.io>
2023-06-01 17:04:26 -0500

    working on this appbase again

--- backend/api/main.py
+++ backend/api/main.py
@@ -35,7 +35,7 @@ auth_handler = Auth()
 @app.post('/v1/signup', response_model=UserAuthResponseModel)
 def signup_api(user_details: SignUpRequestModel):
     """
-    This sign-up API allow you to register your account, and return access token.
+    This sign-up API allows you to register your account and returns an access token.
     """
     user = register_user(user_details)
     access_token = auth_handler.encode_token(user_details.email)
@@ -46,61 +46,31 @@ def signup_api(user_details: SignUpRequestModel):
 @app.post('/v1/signin', response_model=UserAuthResponseModel)
 def signin_api(user_details: SignInRequestModel):
     """
-    This sign-in API allow you to obtain your access token.
+    This sign-in API allows you to obtain your access token.
     """
     user = signin_user(user_details.email, user_details.password)
-    access_token = auth_handler.encode_token(user['email'])
-    refresh_token = auth_handler.encode_refresh_token(user['email'])
+    access_token = auth_handler.encode_token(user.email)
+    refresh_token = auth_handler.encode_refresh_token(user.email)
     return JSONResponse(status_code=200, content={'token': {'access_token': access_token, 'refresh_token': refresh_token}, 'user': user})
 
-
-@app.get('/v1/refresh-token')
-def refresh_token_api(refresh_token: str):
-    """
-    This refresh-token API allow you to obtain new access token.
-    """
-    new_token = auth_handler.refresh_token(refresh_token)
-    return {'access_token': new_token}
 
 
 ################################
 ########## Users APIs ##########
 ################################
 
-@app.get("/v1/users", response_model=list[UserResponseModel])
-def get_all_users_api(credentials: HTTPAuthorizationCredentials = Security(security)):
-    """
-    This users get API allow you to fetch all user data.
-    """
-    token = credentials.credentials
-    if (auth_handler.decode_token(token)):
-        user = get_all_users()
-        return JSONResponse(status_code=200, content=jsonable_encoder(user))
-    return JSONResponse(status_code=401, content={'error': 'Faild to authorize'})
-
-
-@app.get("/v1/user/{user_id}", response_model=UserResponseModel)
-def get_user_api(user_id: int, credentials: HTTPAuthorizationCredentials = Security(security)):
-    """
-    This user API allow you to fetch specific user data.
-    """
-    token = credentials.credentials
-    if (auth_handler.decode_token(token)):
-        user = get_user_by_id(user_id)
-        return JSONResponse(status_code=200, content=jsonable_encoder(user))
-    return JSONResponse(status_code=401, content={'error': 'Faild to authorize'})
 
 
 @app.post("/v1/user/update", response_model=UserResponseModel)
 def update_user_api(user_details: UserUpdateRequestModel, credentials: HTTPAuthorizationCredentials = Security(security)):
     """
-    This user update API allow you to update user data.
+    This user update API allows you to update user data.
     """
     token = credentials.credentials
     if (auth_handler.decode_token(token)):
         user = update_user(user_details)
-        return JSONResponse(status_code=200, content=jsonable_encoder(user))
-    return JSONResponse(status_code=401, content={'error': 'Faild to authorize'})
+        return {'user': user}
+    raise HTTPException(status_code=401, detail='Failed to authorize')
 
 
 ###############################
--- backend/api/user/user.py
+++ backend/api/user/user.py
@@ -2,102 +2,88 @@ from fastapi import HTTPException
 from database.query import query_get, query_put, query_update
 from .auth import Auth
 from .models import UserUpdateRequestModel
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import MultipleResultsFound
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth_handler = Auth()
 
 
 def register_user(user_model: UserUpdateRequestModel):
-    user = get_user_by_email(user_model.email)
-    if len(user) != 0:
-        raise HTTPException(
-            status_code=409, detail='Email user already exist.')
-    hashed_password = auth_handler.encode_password(user_model.password)
-    query_put("""
-                INSERT INTO user (
-                    first_name,
-                    last_name,
-                    email,
-                    password_hash
-                ) VALUES (%s,%s,%s,%s)
-                """,
-              (
-                  user_model.first_name,
-                  user_model.last_name,
-                  user_model.email,
-                  hashed_password
-              )
-              )
-    user = get_user_by_email(user_model.email)
-    return user[0]
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        user = session.query(User).filter_by(email=user_model.email).one_or_none()
+        if user is not None:
+            raise HTTPException(
+                status_code=409, detail='Email user already exists.')
+        hashed_password = auth_handler.encode_password(user_model.password)
+        user = User(
+            first_name=user_model.first_name,
+            last_name=user_model.last_name,
+            email=user_model.email,
+            password_hash=hashed_password
+        )
+        session.add(user)
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+    return user
 
 
-def signin_user(email, password):
-    user = get_user_by_email(email)
-    if len(user) == 0:
-        print('Invalid email')
-        raise HTTPException(status_code=401, detail='Invalid email')
-    if (not auth_handler.verify_password(password, user[0]['password_hash'])):
-        print('Invalid password')
-        raise HTTPException(status_code=401, detail='Invalid password')
-    return user[0]
+def signin_user(email: str, password: str) -> User:
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        if not check_password_hash(user.password_hash, password):
+            raise Exception("Invalid password")
+    except NoResultFound:
+        raise Exception(f"No user found with email {email}")
+    except MultipleResultsFound:
+        raise Exception(f"Multiple users found for email {email}")
+    finally:
+        session.close()
+    return user
 
 
 def update_user(user_model: UserUpdateRequestModel):
-    hashed_password = auth_handler.encode_password(user_model.password)
-    query_put("""
-            UPDATE user 
-                SET first_name = %s,
-                    last_name = %s,
-                    email = %s,
-                    password_hash = %s 
-                WHERE user.email = %s;
-            """,
-              (
-                  user_model.first_name,
-                  user_model.last_name,
-                  user_model.email,
-                  hashed_password,
-                  user_model.email,
-              )
-              )
-    user = get_user_by_email(user_model.email)
-    return user[0]
-
-
-def get_all_users():
-    user = query_get("""
-        SELECT  
-            user.id,
-            user.first_name,
-            user.last_name,
-            user.email
-        FROM user
-        """, ())
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        user = session.query(User).filter_by(email=user_model.email).one_or_none()
+        if user is None:
+            raise HTTPException(
+                status_code=404, detail='User not found.')
+        hashed_password = auth_handler.encode_password(user_model.password)
+        user.first_name = user_model.first_name
+        user.last_name = user_model.last_name
+        user.email = user_model.email
+        user.password_hash = hashed_password
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
     return user
+
 
 
 def get_user_by_email(email: str):
-    user = query_get("""
-        SELECT 
-            user.id,
-            user.first_name,
-            user.last_name,
-            user.email,
-            user.password_hash
-        FROM user 
-        WHERE email = %s
-        """, (email))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        user = session.query(User).filter_by(email=email).one()
+    except NoResultFound:
+        user = None
+    except MultipleResultsFound:
+        raise Exception(f"Multiple users found for email {email}")
+    finally:
+        session.close()
     return user
 
-
-def get_user_by_id(id: int):
-    user = query_get("""
-        SELECT 
-            user.id,
-            user.first_name,
-            user.last_name,
-            user.email,
-        FROM user 
-        WHERE id = %s
-        """, (id))
-    return user
